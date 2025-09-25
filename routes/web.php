353b683092cbd735cce529c5078fe8374c63608e
muhammadilhamyahya222 <62550125+muhammadilhamyahya222@users.noreply.github.com>
2025-09-25 09:50:49 +0700

    feat: slicing sign in and sign up

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome'); // resources/js/Pages/Welcome.jsx
+Route::redirect('/', '/prototype/login');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    Route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+    Route::get('/register', function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
