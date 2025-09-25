64518a31486877c644f5bd4ec8bb2ea6f2675399
muhammadilhamyahya222 <62550125+muhammadilhamyahya222@users.noreply.github.com>
2025-09-25 23:48:28 +0700

    feat: slicing detail movie

--- resources/js/Components/FeaturedMovie.jsx
+++ resources/js/Components/FeaturedMovie.jsx
@@ -1,3 +1,4 @@
+import { Link } from "@inertiajs/react";
 import PropTypes from "prop-types";
 
 export default function FeaturedMovie({
@@ -41,7 +42,7 @@ export default function FeaturedMovie({
                     <img src="/icons/ic_play.svg" width="50" alt="" />
                 </div>
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link href={route('prototype.movie.show', slug)} className="inset-0 absolute z-50"></Link>
         </div>
     );
 }
--- resources/js/Components/MovieCard.jsx
+++ resources/js/Components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "@inertiajs/react";
 import PropTypes from "prop-types";
 
 export default function MovieCard( {
@@ -26,7 +27,7 @@ export default function MovieCard( {
             >
                 <img src="/icons/ic_play.svg" className="" width="50" alt="" />
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link href={route('prototype.movie.show', slug)} className="inset-0 absolute z-50"></Link>
         </div>
     );
 }
