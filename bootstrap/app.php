bf474aa08b5b11b36f9bf6038c57298cf9655421
muhammadilhamyahya222 <62550125+muhammadilhamyahya222@users.noreply.github.com>
2025-10-01 11:49:41 +0700

    feat: integrate midtrans

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'checkUserSubscription' => \App\Http\Middleware\CheckUserSubscription::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'midtrans/notification',
+        ]);
 
         //
     })
